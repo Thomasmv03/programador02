f161fa855c39b17560778ffb046e8a3ff813f196
Thomas Montenegro <thomi14mb@gmail.com>
2022-06-27 14:12:18 -0300

    retoques

--- M4U4/public/javascripts/form.js
+++ M4U4/public/javascripts/form.js
@@ -14,6 +14,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }).then(function (response) {
             response.json().then(function (obj) {
                 feedback.innerText = obj.message;
+                form.reset();
             })
         });
     });
